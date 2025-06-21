1c1ea29cf7d846157ab351248a97cf0827c1d596
Haad majeed <haadmajeedch27@gmail.com>
2025-06-22 04:57:16 +0500

    start of scroll trgger website

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ScrollTrigger,SplitText } from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+//split text for text animation and the scrool trogger for scroll animatin
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+    return (
+        <div className='flex-center h-[100vh]'>
+            <h1 className='text-3xl text-indigo-300' >
+                Hello gsap
+            </h1>
+        </div>
+    )
+}
+
+export default App
